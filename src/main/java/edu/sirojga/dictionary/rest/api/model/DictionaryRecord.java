56e017d78815f0a12e0f188b8fa144bd761b4bfd
sirojga <sirojga@gmail.com>
2021-07-23 16:55:20 +0300

    add dictionary record

---
+++ src/main/java/edu/sirojga/dictionary/rest/api/model/DictionaryRecord.java
@@ -0,0 +1,22 @@
+package edu.sirojga.dictionary.rest.api.model;
+
+import java.util.List;
+
+public class DictionaryRecord {
+
+    final String sourceLanguage;
+    final String translateLanguage;
+    final String word;
+    final String translation;
+
+    public DictionaryRecord(String sourceLanguage, String translateLanguage, String word, String translation) {
+        this.sourceLanguage = sourceLanguage;
+        this.translateLanguage = translateLanguage;
+        this.word = word;
+        this.translation = translation;
+    }
+
+    public List<String> record(){
+        return List.of(sourceLanguage,translateLanguage,word,translation);
+    }
+}
